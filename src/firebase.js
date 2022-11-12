7d667a6747539998c749ab7bebe759340609dce7
prabhakarzha <kumarprabhakar141@gmail.com>
2022-11-12 11:51:36 +0530

    frontend part

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAHRwmsvSgnPL_Xk4-fvZUdvQN_KNOH18Y",
+  authDomain: "whatsap-mern-22d1e.firebaseapp.com",
+  projectId: "whatsap-mern-22d1e",
+  storageBucket: "whatsap-mern-22d1e.appspot.com",
+  messagingSenderId: "545441379375",
+  appId: "1:545441379375:web:107732d2bbc12cd4fdc2da",
+  measurementId: "G-RMQ5SENPVW",
+};
